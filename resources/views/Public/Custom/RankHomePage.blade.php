731fd3b672b2d13540d33b4317a0aae2d3e7f1c8
lu <luxolo.mangcotywa@gmail.com>
2019-05-04 19:19:57 +0200

    Adding custom text

--- resources/views/Public/Custom/RankHomePage.blade.php
+++ resources/views/Public/Custom/RankHomePage.blade.php
@@ -100,7 +100,7 @@
                         <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="300ms">
                             {!!HTML::image('assets/custom/rank/images/rank/icon1.png')!!}
                         </div>
-                        <h2>Incredibly Responsive</h2>
+                        <h2>Instant Message</h2>
                         <p>Ground round tenderloin flank shank ribeye. Hamkevin meatball swine. Cow shankle beef sirloin chicken ground round.</p>
                     </div>
                 </div>
@@ -109,7 +109,7 @@
                         <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="600ms">
                             {!!HTML::image('assets/custom/rank/images/rank/icon2.png')!!}
                         </div>
-                        <h2>Superior Typography</h2>
+                        <h2>Information Distribution</h2>
                         <p>Hamburger ribeye drumstick turkey, strip steak sausage ground round shank pastrami beef brisket pancetta venison.</p>
                     </div>
                 </div>
@@ -118,7 +118,7 @@
                         <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="900ms">
                             {!!HTML::image('assets/custom/rank/images/rank/icon3.png')!!}
                         </div>
-                        <h2>Swift Page Builder</h2>
+                        <h2>Free marketing</h2>
                         <p>Venison tongue, salami corned beef ball tip meatloaf bacon. Fatback pork belly bresaola tenderloin bone pork kevin shankle.</p>
                     </div>
                 </div>
@@ -133,12 +133,12 @@
                 <div class="row">
                     <div class="action take-tour">
                         <div class="col-sm-7 wow fadeInLeft" data-wow-duration="500ms" data-wow-delay="300ms">
-                            <h1 class="title">Triangle Corporate Template</h1>
-                            <p>A responsive, retina-ready &amp; wide multipurpose template.</p>
+                            <h1 class="title">Put Your rank in the map</h1>
+                            <p>Let the world know your ranks whereabouts.</p>
                         </div>
                         <div class="col-sm-5 text-center wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">
                             <div class="tour-button">
-                                <a href="#" class="btn btn-common">TAKE THE TOUR</a>
+                                <a href="#" class="btn btn-common">Add a new rank</a>
                              </div>
                         </div>
                     </div>
@@ -147,7 +147,15 @@
         </div>
    </section>
     <!--/#action-->
-
+    <section id="about-company" class="padding-top wow fadeInUp" data-wow-duration="400ms" data-wow-delay="400ms">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                    <h1 class="margin-bottom">Why sell tickets online</h1>
+                </div>
+            </div>
+        </div>
+    </section>
     <section id="features">
         <div class="container">
             <div class="row">
@@ -156,13 +164,13 @@
                         {!!HTML::image('assets/custom/rank/images/rank/image1.png','', ['class' => 'img-responsive'])!!}
                     </div>
                     <div class="col-sm-6 wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">
-                        <h2>Experienced and Enthusiastic</h2>
+                        <h2>Increase Revenue</h2>
                         <P>Pork belly leberkas cow short ribs capicola pork loin. Doner fatback frankfurter jerky meatball pastrami bacon tail sausage. Turkey fatback ball tip, tri-tip tenderloin drumstick salami strip steak.</P>
                     </div>
                 </div>
                 <div class="single-features">
                     <div class="col-sm-6 col-sm-offset-1 align-right wow fadeInLeft" data-wow-duration="500ms" data-wow-delay="300ms">
-                        <h2>Built for the Responsive Web</h2>
+                        <h2>Quick and Easy Checkout Process</h2>
                         <P>Mollit eiusmod id chuck turducken laboris meatloaf pork loin tenderloin swine. Pancetta excepteur fugiat strip steak tri-tip. Swine salami eiusmod sint, ex id venison non. Fugiat ea jowl cillum meatloaf.</P>
                     </div>
                     <div class="col-sm-5 wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">
@@ -174,7 +182,7 @@
                         {!!HTML::image('assets/custom/rank/images/rank/image3.png','', ['class' => 'img-responsive'])!!}
                     </div>
                     <div class="col-sm-6 wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">
-                        <h2>Experienced and Enthusiastic</h2>
+                        <h2>Customizable Tickets - with QR Codes</h2>
                         <P>Ut officia cupidatat anim excepteur fugiat cillum ea occaecat rump pork chop tempor. Ut tenderloin veniam commodo. Shankle aliquip short ribs, chicken eiusmod exercitation shank landjaeger spare ribs corned beef.</P>
                     </div>
                 </div>
@@ -189,8 +197,8 @@
                 <div class="col-sm-12">
                     <div class="clients text-center wow fadeInUp" data-wow-duration="500ms" data-wow-delay="300ms">
                         <p>{!!HTML::image('assets/custom/rank/images/rank/clients.png','', ['class' => 'img-responsive'])!!}</p>
-                        <h1 class="title">Happy Clients</h1>
-                        <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br> Ut enim ad minim veniam, quis nostrud </p>
+                        <h1 class="title">Say Hi To <b>QAXE</b> Tickets Selling System</h1>
+                        <p>Start selling today</p>
                     </div>
                     <div class="clients-logo wow fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
                         <div class="col-xs-3 col-sm-2">
