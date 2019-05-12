593d91879bcc4dc8fbcd71f751143c1ab6709552
lu <luxolo.mangcotywa@gmail.com>
2019-05-12 12:59:25 +0200

    Hides code comments showing on the events page

--- resources/views/Public/ViewEvent/EventPageCheckout.blade.php
+++ resources/views/Public/ViewEvent/EventPageCheckout.blade.php
@@ -2,10 +2,6 @@
 
 @section('head')
     <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
-    /** Lu: Adding custom script for qaxe */
-    <script type="text/javascript" src="/assets/custom/common/js/jquery.js"></script>
-    <script type="text/javascript" src="/assets/custom/common/js/holder-profile-photo.js"></script>
-    /** End adding custom scripts */
 @stop
 
 @section('content')
@@ -16,3 +12,9 @@
     @include('Public.ViewEvent.Partials.EventFooterSection')
 @stop
 
+{{--- Lu: Adding custom script for qaxe ---}}
+
+@section('footer')
+    <script type="text/javascript" src="/assets/custom/common/js/jquery.js"></script>
+    <script type="text/javascript" src="/assets/custom/common/js/holder-profile-photo.js"></script>
+@stop
